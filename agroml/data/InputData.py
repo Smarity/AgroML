498d2126924305cc386e2499bc268da9846f53a8
juanantoniobellido <jabellido1@gmail.com>
2022-12-01 11:56:56 +0100

    InputData class and its test modified

--- agroml/data/InputData.py
+++ agroml/data/InputData.py
@@ -4,12 +4,40 @@ import pandas as pd
 
 class InputData:
 
-    def __init__(self, fileLocation):
-        assert self._doesFileExists(fileLocation), "File does not exist"
+    def __init__(self, fileLocation:str):
+        self.fileLocation = fileLocation
 
-    def _doesFileExists(fileLocation) -> bool:
-        return os.path.exists(fileLocation)
+        assert self._doesFileExists()
 
-    def _getFileExtension(fileLocation) -> str:
-        return os.path.splitext(fileLocation)[-1]
+        # read the data into a pandas dataframe
+        if self._getFileExtension() in set([".xls", ".xlsx"]):
+            self.readDataExcel()
+        else: 
+            self.readDataCsv()
+
+        # get attributes from data
+        self.columnNamesList = list(self.data.columns)
+        self.nColumns = self.data.shape[1]
+
+
+    def _doesFileExists(self) -> bool:
+        return os.path.exists(self.fileLocation)
+
+    def _getFileExtension(self) -> str:
+        return os.path.splitext(self.fileLocation)[-1]
+
+    def readDataExcel(self) -> pd.DataFrame:
+        self.data = pd.read_excel(self.fileLocation)
+        
+        
+    def readDataCsv(self) -> pd.DataFrame:
+        for sep in [",", ";", r"\s+"]: # r"\s+" must be written this way not to give a warning
+            self.data = pd.read_csv(self.fileLocation, sep=sep)
+            self.nColumns = self.data.shape[1]
+            if self.nColumns > 1:
+                break
+        else:
+            # the r"" (raw string) is used in the message because \s+ gives a warning
+            assert(True, r"""Please, use any of the following separators [",", ";", "\s+"]""")
+
 
--- tests/test_InputData.py
+++ tests/test_InputData.py
@@ -1,21 +1,40 @@
-from agroml.data.InputData import InputData
+import os
 
-from icecream import icecream
+from icecream import ic
+
+from agroml.data import InputData
 
 def test_errorForNullFile():
+
     try:
         inputData = InputData("nullFile.csv")
     except Exception as error:
         assert True
     
 def test_getCorrectFileExtension():
-    
-    
-    inputData = InputData("tests/testData/dataExample.csv")
-    assert inputData._getFileExtension() == ".csv"
 
-    inputData = InputData("tests/testData/dataExample.xlsx")
-    assert inputData._getFileExtension() == ".xlsx"
+    inputFilesList = [
+        "tests/testData/dataExample.csv",
+        "tests/testData/dataExample.xls",
+        "tests/testData/dataExample.txt"
+    ]
+    fileExtensionList = [
+        ".csv", ".xls", ".txt"
+    ]
+    for file, extension in zip(inputFilesList, fileExtensionList):
+        inputData = InputData(file)
+        assert inputData._getFileExtension() == extension
     
-    inputData = InputData("tests/testData/dataExample.txt")
-    assert inputData._getFileExtension() == ".txt"
+def test_readDataCsv():
+    allDataFiles = os.listdir("tests/testData")
+
+    variableList = [
+        "station","day","year","date","tx","tm","tn","rhx","rhm","rhn","rs","et0"
+    ]
+
+    for file in allDataFiles:
+        inputData = InputData("tests/testData/" + file)
+        assert inputData.nColumns == len(variableList)
+        ic(inputData.columnNamesList)
+        ic(variableList)
+        assert inputData.columnNamesList == variableList
