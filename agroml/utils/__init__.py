d727a5e3dd9b3ec14706d2f1836bba378bb3c893
juanantoniobellido <jabellido1@gmail.com>
2023-03-06 10:32:25 +0100

    feat: Include statistics in the __init__ to be imported easily

--- agroml/utils/__init__.py
+++ agroml/utils/__init__.py
@@ -1,5 +1,12 @@
 from doNotRunItTwice import doNotRunItTwice
-
+from statistics import getMeanBiasError
+from statistics import getRootMeanSquaredError
+from statistics import getMeanAbsoluteError
+from statistics import getCoefficientOfDetermination
+from statistics import getNashSuteliffeEfficiency
+from statistics import getLinearRegression
 __all__ = [
-    'doNotRunItTwice'
+    'doNotRunItTwice', "getMeanBiasError", "getRootMeanSquaredError", 
+    "getMeanAbsoluteError", "getCoefficientOfDetermination", "getNashSuteliffeEfficiency", 
+    "getLinearRegression"
 ]
